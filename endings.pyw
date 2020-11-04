bfcf5db6a9983ee80be69877c190f5faebcb2217
Senecm <58762403+Senecm@users.noreply.github.com>
2020-11-05 09:11:44 +1030

    added the endings module

---
+++ endings.pyw
@@ -0,0 +1,11 @@
+from time import sleep
+class endings:
+    def __init__(self):
+        pass
+    def groending(self):
+        print("*****")
+        print("You were met at your door by a man with a wide brimmed hat, and cast down eyes.")
+        print("He pulls out a piece of paper, clearly an offical legal document,")
+        print("and begins to read it out, quickly and softly so you only make out the words 'death' and 'retribution'")
+        print("The man takes out a pair of manacles.")
+        print("*****")
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 #idea concived by Henry
 __author__ = "Gordan"
-
-print("Presenting...")
+from endings import * #importing custom ending module
+from time import sleep
+sh = sleep(0.5)
+#creating a class instance
+end = endings()
+print("Presenting...", sh)
 print("initialising...")
 print("Adventure of The Lemom")
 
-name = input("What is the name of our hero?")
+name = input("What is the name of our hero?: ")
+if name == "grolong" or name == "Grolong":
+    print("The venial sin has been committed.")
+    print("The eternal covenant broken.")
+    print("Be gone traitor.")
+    end.groending()
 print("so be it.")
 print("Our story begins in the wilderness")
