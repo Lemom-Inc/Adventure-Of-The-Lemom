3ed78e757c0463f53080d3c1417203d29b71dece
Senecm <58762403+Senecm@users.noreply.github.com>
2020-11-04 17:48:07 +1030

    Added

---
+++ main.py
@@ -0,0 +1,10 @@
+#idea concived by Henry
+__author__ = "Gordan"
+
+print("Presenting...")
+print("initialising...")
+print("Adventure of The Lemom")
+
+name = input("What is the name of our hero?")
+print("so be it.")
+print("Our story begins in the wilderness")
